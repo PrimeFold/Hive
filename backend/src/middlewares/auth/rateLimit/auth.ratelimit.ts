551fe6f251af3d73084a9411d37c73cee7954a3d
Aditya Raj <cyberadi336@gmail.com>
2026-04-28 08:11:50 +0530

    feat : adjusted the rate limit for dev purposes..

--- backend/src/middlewares/auth/rateLimit/auth.ratelimit.ts
+++ backend/src/middlewares/auth/rateLimit/auth.ratelimit.ts
@@ -3,6 +3,6 @@ import  { type RateLimitRequestHandler } from 'express-rate-limit'
 
 export const authLimiter:RateLimitRequestHandler = Limiter({
   windowMs: 60 * 60 * 1000,
-  max: 5,
+  max: 10,
   message: "Login limit reached!",
 });
